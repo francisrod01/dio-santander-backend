f4db773bfead6efe05556395db051f0fd806a353
Francis Rodrigues <francis.rod01@outlook.com>
2021-06-02 00:04:44 -0300

    feat: Implemented stock mapper

--- src/main/java/com/project/bootcamp/mapper/StockMapper.java
+++ src/main/java/com/project/bootcamp/mapper/StockMapper.java
@@ -1,7 +1,32 @@
 package com.project.bootcamp.mapper;
 
+import com.project.bootcamp.dto.StockDTO;
+import com.project.bootcamp.model.Stock;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StockMapper {
+    public Stock toEntity(StockDTO dto) {
+        Stock stock = new Stock();
+
+        stock.setId(dto.getId());
+        stock.setName(dto.getName());
+        stock.setPrice(dto.getPrice());
+        stock.setVariation(dto.getVariation());
+        stock.setDate(dto.getDate());
+
+        return stock;
+    }
+
+    public StockDTO toDto(Stock stock) {
+        StockDTO dto = new StockDTO();
+
+        dto.setId(stock.getId());
+        dto.setName(stock.getName());
+        dto.setPrice(stock.getPrice());
+        dto.setVariation(stock.getVariation());
+        dto.setDate(stock.getDate());
+
+        return dto;
+    }
 }
