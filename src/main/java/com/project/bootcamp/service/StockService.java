f86d44c28b6500dab66270a44b569dddb121d45e
Francis Rodrigues <francis.rod01@outlook.com>
2021-06-02 03:29:55 -0300

    enh: Check if stock exists before saving it

--- src/main/java/com/project/bootcamp/service/StockService.java
+++ src/main/java/com/project/bootcamp/service/StockService.java
@@ -1,9 +1,13 @@
 package com.project.bootcamp.service;
 
+import java.util.Optional;
+
 import com.project.bootcamp.dto.StockDTO;
+import com.project.bootcamp.exceptions.BusinessException;
 import com.project.bootcamp.mapper.StockMapper;
 import com.project.bootcamp.model.Stock;
 import com.project.bootcamp.repository.StockRepository;
+import com.project.bootcamp.util.MessageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +23,11 @@ public class StockService {
 
     @Transactional
     public StockDTO save(StockDTO dto) {
+        Optional<Stock> optional = repository.findByNameAndDate(dto.getName(), dto.getDate());
+        if (optional.isPresent()) {
+            throw new BusinessException(MessageUtils.STOCK_ALREADY_EXISTS);
+        }
+
         Stock stock = mapper.toEntity(dto);
         repository.save(stock);
         return mapper.toDto(stock);
